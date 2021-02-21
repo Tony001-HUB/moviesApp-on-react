78418c51cf4430276a77f6cc61493d647ec5a438
Anton <Osipenko>
2021-02-21 10:02:49 +0300

    Training mini-tasks, working with data entry forms

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React from 'react';
-import Posts from './components/posts'
+import Form from './components/form.js'
 
-export default class App extends React.Component {
-  state = {
-      posts: [
-        {id: 'xcv44', name: `Home1`},
-        {id: 'xcv45', name: 'Home2'},
-        {id: 'xcv46', name: 'Home3'}
-      ]
-  };
-
-  removePost = (id) => {
-    this.setState({posts: this.state.posts.filter(post => post.id !== id)})
-  }
-
-  
-  render() {
-      return (
-        <div className="App">
-            {<Posts posts={this.state.posts} removePost={this.removePost}/>}
-        </div>
-      );
-  }
+function App() {
+  return(
+    <div className='App'>
+      <Form/>
+    </div>
+  )
 }
+
+export default App;
--- src/components/post.js
+++
@@ -1,7 +0,0 @@
-export default function Post(props) {
-    return (
-    <h3>
-        {props.name} <button onClick={() => props.removePost(props.id)}>delete</button>
-    </h3>
-    )
-}
